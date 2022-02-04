b013fd60348c01e71b6342eda7f928ac90a25620
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-02-05 01:13:15 +0800

    Import MatFormFieldModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './pages/products/products.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [AppComponent, ProductsComponent],
   imports: [
+    MatFormFieldModule,
     MatButtonModule,
     MatCardModule,
     BrowserModule,
