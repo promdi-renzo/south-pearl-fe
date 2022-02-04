7b4f8547834c32f8512aeebfd49e3f89944e6922
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-02-05 01:10:31 +0800

    Import MatButtonModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './pages/products/products.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, ProductsComponent],
   imports: [
+    MatButtonModule,
     MatCardModule,
     BrowserModule,
     AppRoutingModule,
